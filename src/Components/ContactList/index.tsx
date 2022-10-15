724c9cc37de2d7501f84f9913b8661b82bd5e079
bertogz <bertogzlx@gmail.com>
2022-10-15 11:14:40 -0700

    update contact list item styling

--- src/Components/ContactList/index.tsx
+++ src/Components/ContactList/index.tsx
@@ -1,15 +1,26 @@
+import { Stack } from "@mui/system";
 import React from "react";
-import { users } from "../../data";
+import { users } from "../../Data";
+import { setSelectedProfile } from "../../Redux/AppState";
+import { useAppDispatch } from "../../Redux/useAppDispatch";
 import ContactListItem from "../ContactListItem";
 
-
 const ContactList = () => {
+  const dispatch = useAppDispatch();
+  function onClick(user_id: number) {
+    dispatch(setSelectedProfile(user_id));
+  }
   return (
-    <>
+    <Stack direction={'column'} flex={1}>
       {users.map((item) => (
-        <ContactListItem {...{ item }} />
+        <ContactListItem
+          {...{ item }}
+          onClick={() => {
+            onClick(item.user_id);
+          }}
+        />
       ))}
-    </>
+    </Stack>
   );
 };
 export default ContactList;
--- src/Components/ContactListItem/index.tsx
+++ src/Components/ContactListItem/index.tsx
@@ -1,21 +1,40 @@
 import React from "react";
-import { Avatar, Typography } from "@mui/material";
+import {
+  Avatar,
+  ButtonBase,
+  Typography,
+} from "@mui/material";
 import { Stack } from "@mui/system";
 
-const ContactListItem = ({ item }: { item: TypeDataEntityContact }) => {
+const ContactListItem = ({
+  item,
+  onClick,
+}: {
+  item: TypeDataEntityContact;
+  onClick: () => void;
+}) => {
   const { profilepic, firstname, lastname, role } = item || {};
   if (!firstname) {
     return <></>;
   }
   const fullname = `${firstname} ${lastname}`;
   return (
-    <Stack direction="row" alignItems="center">
-      <Avatar alt={fullname} src={profilepic} />
-      <Stack>
-        <Typography fontWeight={'bold'} textAlign='left'>{fullname}</Typography>
-        <Typography textAlign='left'>{role}</Typography>
+    <ButtonBase sx={{ justifyContent: "flex-start" }} onClick={onClick}>
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent={"space-between"}
+        spacing={1}
+      >
+        <Avatar alt={fullname} src={profilepic} sx={{ margin: 1 }} />
+        <Stack flex={1}>
+          <Typography textAlign={"left"} fontWeight={"bold"}>
+            {fullname}
+          </Typography>
+          <Typography textAlign={"left"} color="GrayText">{role}</Typography>
+        </Stack>
       </Stack>
-    </Stack>
+    </ButtonBase>
   );
 };
 export default ContactListItem;
