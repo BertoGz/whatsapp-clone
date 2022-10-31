a11f1ce4f024b2fc8a689ddc2282af9da0d78cf4
bertogz <bertogzlx@gmail.com>
2022-10-31 15:16:13 -0700

    continue work on useQbSession

--- src/Quickblox/useQbSession.js
+++ src/Quickblox/useQbSession.js
@@ -1,47 +1,107 @@
 import * as QB from "quickblox/quickblox";
 import { useEffect } from "react";
 import { PromisedQb } from ".";
-import { setQbInitFailure, setQbInitSuccess } from "../Redux/Quickblox";
+import { useQueryUserData } from "../ReactQuery/Queries/useQueryUserData";
+import {
+  setAppSessionInvalid,
+  setAppSessionValid,
+  setQbInitSuccess,
+  setUserSessionInValid,
+  setUserSessionValid,
+} from "../Redux/Quickblox";
 import { useAppDispatch } from "../Redux/useAppDispatch";
 import { useAppSelector } from "../Redux/useAppSelector";
 var APPLICATION_ID = 98550;
 var AUTH_KEY = "rPQMBraVeQN6Gpd";
 var AUTH_SECRET = "gFYev7gKMzmAAcG";
 var ACCOUNT_KEY = "ub3Nry9YQWQHnWQqLKez";
-var CONFIG = { debug: true };
+var CONFIG = { debug: false };
 
 export const useQbSession = () => {
-  const { qbInitialized } = useAppSelector((state) => state.Quickblox);
+  const { data: userData } = useQueryUserData();
+  const { email, uid } = userData || {};
+  const { qbInitialized, appSessionValid, userSessionValid } = useAppSelector(
+    (state) => state.Quickblox
+  );
   const dispatch = useAppDispatch();
   async function tryQbInit() {
     await QB.init(APPLICATION_ID, AUTH_KEY, AUTH_SECRET, ACCOUNT_KEY, CONFIG);
-    //create app session
+    dispatch(setQbInitSuccess());
+  }
+  async function tryCreateAppSession() {
     await PromisedQb.createSession()
       .then(() => {
-        dispatch(setQbInitSuccess());
+        dispatch(setAppSessionValid());
       })
       .catch(() => {
-        dispatch(setQbInitFailure());
+        dispatch(setAppSessionInvalid());
       });
+
   }
   async function tryCreateUserSession() {
     // check if application session has been created
     const session = await PromisedQb.getSession();
-    console.log("hasSession", session);
-    if (session?.application_id){
-        const params = {}
-        const userSession = PromisedQb.createSession(params)
+    console.log("!!@@session", session);
+    // early return if user session exists
+    if (session?.application_id && session?.user_id !== 0 && session?.user_id) {
+      dispatch(setUserSessionInValid());
+      return;
+    }
+    if (
+      session?.application_id &&
+      (session?.user_id === 0 || !session?.user_id)
+    ) {
+      console.log("!!@@user has application session", session);
+      const loginParams = { login: email, password: uid };
+
+      let userExists = null;
+
+      await PromisedQb.loginUser(loginParams)
+        .then(() => {
+          userExists = true;
+          dispatch(setUserSessionValid());
+        })
+        .catch((err) => {
+          const { code } = err || {};
+
+          if (code === 401) {
+            userExists = false;
+          } else {
+            dispatch(setUserSessionInValid());
+          }
+        });
+
+      if (userExists === false) {
+        const createParams = {
+          login: email,
+          password: uid,
+          full_name: "QuickBlox Test",
+        };
+
+        const newUser = await PromisedQb.createUser(createParams);
+        if (newUser) {
+          await PromisedQb.loginUser(loginParams);
+          dispatch(setUserSessionValid());
+        }
+      }
+  
     }
   }
   useEffect(() => {
-    if (qbInitialized === null) {
+    if (qbInitialized === null && userData) {
       tryQbInit();
     }
-  }, [qbInitialized]);
+  }, [qbInitialized, userData]);
 
   useEffect(() => {
-    if (qbInitialized) {
+    if (qbInitialized && !appSessionValid) {
+      tryCreateAppSession();
+    }
+  }, [qbInitialized, appSessionValid]);
+
+  useEffect(() => {
+    if (appSessionValid && !userSessionValid) {
       tryCreateUserSession();
     }
-  }, [qbInitialized]);
+  }, [appSessionValid, userSessionValid]);
 };
