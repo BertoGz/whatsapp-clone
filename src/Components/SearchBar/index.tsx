c788a858c740cfb563c95fc0a171828bad687419
bertogz <bertogzlx@gmail.com>
2022-10-31 15:12:30 -0700

    add popover on searchbar

--- src/Components/SearchBar/index.tsx
+++ src/Components/SearchBar/index.tsx
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
-import { ButtonBase, OutlinedInput } from "@mui/material";
+import {
+  ButtonBase,
+  OutlinedInput,
+  Popover,
+} from "@mui/material";
 import { Stack } from "@mui/system";
-import { Add, Search } from "@mui/icons-material";
+import { Add, AddBox, Notifications, Search } from "@mui/icons-material";
+import { RequestsModal } from "../../Containers/RequestsModal";
 
 const SearchBar = () => {
   const [input, setInput] = useState("");
+  //const [showRequestsModal, setShowRequestsModal] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  function handleToggleRequestsModal(
+    event: React.MouseEvent<HTMLButtonElement>
+  ) {
+    if (anchorEl) {
+      setAnchorEl(null);
+      return;
+    }
+    setAnchorEl(event.currentTarget);
+  }
   return (
-    <Stack direction="row" alignItems='center'>
+    <Stack direction="row" alignItems="center">
       <OutlinedInput
         id="outlined-basic"
         label="Outlined"
@@ -15,9 +31,28 @@ const SearchBar = () => {
           const updateVal = e.target.value;
           setInput(updateVal);
         }}
-        startAdornment={<Search/>}
+        startAdornment={<Search />}
       />
-      <ButtonBase><Add /></ButtonBase>
+      <ButtonBase>
+        <AddBox color="action" />
+      </ButtonBase>
+      <ButtonBase onClick={handleToggleRequestsModal}>
+        <Notifications color="warning" />
+      </ButtonBase>
+
+      <Popover
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={() => {
+          setAnchorEl(null);
+        }}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+      >
+        <RequestsModal />
+      </Popover>
     </Stack>
   );
 };
