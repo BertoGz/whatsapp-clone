3e27d0e3fb46b1787f0d342826453798c750ddb9
bertogz <bertogzlx@gmail.com>
2022-11-03 00:52:12 -0700

    update default exports, refetching options set to false

--- src/ReactQuery/index.ts
+++ src/ReactQuery/index.ts
@@ -1,18 +1,22 @@
 import { QueryClient } from "react-query";
-import {User} from 'firebase/auth'
+import { User } from "firebase/auth";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       cacheTime: Infinity,
+      refetchOnWindowFocus:false
     },
   },
 });
 
-export * from './Mutations/useMutationLoginUser'
-export * from './Mutations/useMutationCreateUser'
-
+export * from "./Mutations/useMutationLoginUser";
+export * from "./Mutations/useMutationCreateUser";
+export * from "./Queries/useQueryFirebaseUserData";
+export * from "./Queries/useQueryQuickbloxUserData";
 
 export const clientData = {
-  getUserData:()=> queryClient.getQueryData('userData') as User,
-  getUserCreated:()=>queryClient.getQueryData('userCreated') as User,
-}
+  getFirebaseUserData: () =>
+    queryClient.getQueryData("firebaseUserData") as User,
+  getQuickbloxUserData: () => queryClient.getQueryData("qbUserData") as any,
+  getContacts: () => queryClient.getQueryData("contacts") as Array<any>,
+};
