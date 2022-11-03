4d9895aab536a6a2d7f62a33e49fb3d1a2c644a3
bertogz <bertogzlx@gmail.com>
2022-11-03 00:49:51 -0700

    began calling contacts query

--- src/Containers/Contacts/index.tsx
+++ src/Containers/Contacts/index.tsx
@@ -1,9 +1,13 @@
+import React from 'react'
 import { Stack } from "@mui/system";
-import React from "react";
 import ContactList from "../../Components/ContactList";
 import SearchBar from "../../Components/SearchBar";
+import { useQueryContacts } from "../../ReactQuery/Queries/useQueryContacts";
 
 const Contacts = () => {
+  const { data } = useQueryContacts();
+
+
   return (
     <Stack>
       <SearchBar />
