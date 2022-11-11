29474845f9056692f01143e765a3e5218802901f
bertogz <bertogzlx@gmail.com>
2022-11-10 21:54:52 -0800

    add init consts file

---
+++ src/Contants.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "http://localhost:3000";
+export const GET_MY_RELATIONSHIPS_ENDPOINT = "/get_my_relationships";
+export const SET_RELATIONSHIP_ENDPOINT = "/set_relationship";
