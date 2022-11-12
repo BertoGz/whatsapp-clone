38eddfa62cf7df6d891e6283bf3617f0a4214288
bertogz <bertogzlx@gmail.com>
2022-11-11 16:08:44 -0800

    add accept mutation from pending list

--- src/Containers/RequestsModal/index.tsx
+++ src/Containers/RequestsModal/index.tsx
@@ -1,21 +1,28 @@
 import { Divider, Stack, Typography } from "@mui/material";
-import { useQueryPendingContacts } from "../../ReactQuery/Queries/useQueryPendingContacts";
-
-const PendingContact = ({ contact }: { contact: TypeDataEntityContact }) => {
-  return <Typography></Typography>;
-};
+import ContactListItem from "../../Components/ContactListItem";
+import { useQueryContacts } from "../../ReactQuery";
+import { useMutationAcceptRequest } from "../../ReactQuery/Mutations/useMutationAcceptRequest";
 
 export const RequestsModal = () => {
-  const { data } = useQueryPendingContacts();
+  const { data } = useQueryContacts({ status: "pending" });
+  const { mutateAsync: acceptRequestMutation } = useMutationAcceptRequest();
   const { items: pendingUsers } = data || {};
+  function onAccept(contact: TypeDataEntityContact) {
+    acceptRequestMutation({
+      relationship_id: contact.relationship.relationship_id,
+      status: 1,
+    });
+  }
 
   return (
     <Stack direction={"column"}>
-      <Stack maxWidth={300} p={5} direction={"column"}>
-        <Typography>Pending Invitations</Typography>
+      <Stack p={5} direction={"column"}>
+        <Typography fontWeight={"bold"}>Pending Invitations</Typography>
         <Divider />
         {pendingUsers &&
-          pendingUsers.map((contact) => <PendingContact {...{ contact }} />)}
+          pendingUsers.map((item) => (
+            <ContactListItem {...{ item, variant: "request", onAccept }} />
+          ))}
       </Stack>
     </Stack>
   );
