a38d859a88268e0f4ed8e0155804d052e51e5a35
bertogz <bertogzlx@gmail.com>
2022-11-11 16:10:42 -0800

    add param types

--- src/ReactQuery/Mutations/useMutationAcceptRequest.ts
+++ src/ReactQuery/Mutations/useMutationAcceptRequest.ts
@@ -1,22 +1,26 @@
 import { useMutation } from "react-query";
 import { queryClient } from "..";
 import { PromisedQb } from "../../Quickblox";
+import { updateRelationship } from "../../Requests";
 
 type errorTypes = {};
-
-async function acceptRequestFn(id: number) {
-  return await PromisedQb.confirmAddRoster(id)
+type params = {
+  relationship_id: number;
+  status: 0 | 1 | 2 | 3;
+};
+async function acceptRequestFn({ relationship_id, status }: params) {
+  return await updateRelationship({ relationship_id, status })
     .then((res) => {
-      debugger;
       return Promise.resolve(res);
     })
     .catch((e) => {
+      debugger;
       return Promise.reject(e);
     });
 }
 const queryKey = ["contacts", "pending"];
 export const useMutationAcceptRequest = () => {
-  return useMutation<any, errorTypes, number>(acceptRequestFn, {
+  return useMutation<any, errorTypes, params>(acceptRequestFn, {
     onSuccess: () => {
       queryClient.invalidateQueries(queryKey);
     },
