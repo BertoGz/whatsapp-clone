21f96a16c41b05177399d07e174000f22ce4c664
bertogz <bertogzlx@gmail.com>
2022-11-14 19:14:50 -0800

    update modal to show pending user that is not initiator

--- src/Containers/RequestsModal/index.tsx
+++ src/Containers/RequestsModal/index.tsx
@@ -1,26 +1,32 @@
 import { Divider, Stack, Typography } from "@mui/material";
+import { useMemo } from "react";
 import ContactListItem from "../../Components/ContactListItem";
 import { useQueryContacts } from "../../ReactQuery";
-import { useMutationAcceptRequest } from "../../ReactQuery/Mutations/useMutationAcceptRequest";
+import { useMutationUpdateRelationship } from "../../ReactQuery";
 
 export const RequestsModal = () => {
-  const { data } = useQueryContacts({ status: "pending" });
-  const { mutateAsync: acceptRequestMutation } = useMutationAcceptRequest();
-  const { items: pendingUsers } = data || {};
+  const { data: pendingUsers } = useQueryContacts({ status: "pending" });
+  const { mutateAsync: updateRelationshipMutation } =
+    useMutationUpdateRelationship();
   function onAccept(contact: TypeDataEntityContact) {
-    acceptRequestMutation({
-      relationship_id: contact.relationship.relationship_id,
-      status: 1,
-    });
+    updateRelationshipMutation(
+      {
+        relationship_id: contact.relationship.relationship_id,
+        status: 1,
+      },
+      { onSuccess: () => {} }
+    );
   }
-
+  const pendingUserRequests = useMemo(() => {
+    return pendingUsers?.filter((user) => !user?.relationship.isInitiator);
+  }, [pendingUsers]);
   return (
     <Stack direction={"column"}>
       <Stack p={5} direction={"column"}>
         <Typography fontWeight={"bold"}>Pending Invitations</Typography>
         <Divider />
-        {pendingUsers &&
-          pendingUsers.map((item) => (
+        {pendingUserRequests &&
+          pendingUserRequests.map((item) => (
             <ContactListItem {...{ item, variant: "request", onAccept }} />
           ))}
       </Stack>
