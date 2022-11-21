5428d607ad7fe69dfa22baa96e8fd3ae2b452bc0
bertogz <bertogzlx@gmail.com>
2022-11-20 23:41:52 -0800

    change styling

--- src/Containers/Contacts/index.tsx
+++ src/Containers/Contacts/index.tsx
@@ -3,16 +3,30 @@ import { Stack } from "@mui/system";
 import ContactList from "../../Components/ContactList";
 import SearchBar from "../../Components/SearchBar";
 import { useQueryContacts } from "../../ReactQuery/Queries/useQueryContacts";
+import { useTheme } from "@mui/material";
 
 const Contacts = () => {
-  const { data } = useQueryContacts();
-  const { items } = data || {};
-
-  console.log("contacts data", data);
+  const theme = useTheme();
+  const { data: contacts } = useQueryContacts();
+  console.log("!!@@cacacacaacacaca", contacts);
   return (
-    <Stack bgcolor={"lightslategrey"} sx={{ minHeight: "100vh" }}>
-      <SearchBar />
-      <ContactList data={items ?? []} />
+    <Stack
+
+      sx={{
+        minHeight: "100vh",
+      }}
+    >
+      <Stack
+        direction="column"
+        sx={{
+          p: 1,
+          borderBottom: 1,
+          borderBottomColor: theme.palette.divider,
+        }}
+      >
+        <SearchBar />
+      </Stack>
+      { <ContactList data={contacts ?? []} />}
     </Stack>
   );
 };
