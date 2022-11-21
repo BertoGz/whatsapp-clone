c07fa7e907c4980ca83868c1dab6a5ecddd946fb
bertogz <bertogzlx@gmail.com>
2022-11-20 23:57:17 -0800

    continue work on requests modal

--- src/Containers/RequestsModal/index.tsx
+++ src/Containers/RequestsModal/index.tsx
@@ -1,6 +1,7 @@
 import { Divider, Stack, Typography } from "@mui/material";
 import { useMemo } from "react";
 import ContactListItem from "../../Components/ContactListItem";
+import PendingContactListItem from "../../Components/PendingContactListItem";
 import { useQueryContacts } from "../../ReactQuery";
 import { useMutationUpdateRelationship } from "../../ReactQuery";
 
@@ -18,7 +19,10 @@ export const RequestsModal = () => {
     );
   }
   const pendingUserRequests = useMemo(() => {
-    return pendingUsers?.filter((user) => !user?.relationship.isInitiator);
+    if (!pendingUsers) {
+      return [];
+    }
+    return pendingUsers.filter((user) => !user?.relationship.isInitiator);
   }, [pendingUsers]);
   return (
     <Stack direction={"column"}>
@@ -26,9 +30,16 @@ export const RequestsModal = () => {
         <Typography fontWeight={"bold"}>Pending Invitations</Typography>
         <Divider />
         {pendingUserRequests &&
-          pendingUserRequests.map((item) => (
-            <ContactListItem {...{ item, variant: "request", onAccept }} />
-          ))}
+          pendingUserRequests.map((item) => {
+            if (!item) {
+              return <></>;
+            }
+            return (
+              <PendingContactListItem
+                {...{ item, variant: "request", onAccept }}
+              />
+            );
+          })}
       </Stack>
     </Stack>
   );
