505e0a43b22533f37880580202c0c33f300de36d
bertogz <bertogzlx@gmail.com>
2022-11-22 23:01:16 -0800

    continue work  on contacts container

--- src/Containers/Contacts/index.tsx
+++ src/Containers/Contacts/index.tsx
@@ -3,30 +3,53 @@ import { Stack } from "@mui/system";
 import ContactList from "../../Components/ContactList";
 import SearchBar from "../../Components/SearchBar";
 import { useQueryContacts } from "../../ReactQuery/Queries/useQueryContacts";
-import { useTheme } from "@mui/material";
+import { Button, useTheme } from "@mui/material";
+import { useMutationLogout } from "../../ReactQuery/Mutations/useMutationLogout";
+import { useNavigate } from "react-router-dom";
 
 const Contacts = () => {
+  const navigate = useNavigate();
   const theme = useTheme();
-  const { data: contacts } = useQueryContacts();
-  console.log("!!@@cacacacaacacaca", contacts);
+  const { data: contacts, isLoading,isIdle } = useQueryContacts();
+
+  const { mutateAsync: signOutMutation } = useMutationLogout();
   return (
     <Stack
-
       sx={{
-        minHeight: "100vh",
+        height: "100%",
+        justifyContent: "space-between",
+        backgroundColor: theme.palette.secondary.light,
       }}
     >
-      <Stack
-        direction="column"
-        sx={{
-          p: 1,
-          borderBottom: 1,
-          borderBottomColor: theme.palette.divider,
+      <div>
+        <Stack
+          direction="column"
+          sx={{
+            p: 1,
+            borderBottom: 1,
+            borderBottomColor: theme.palette.divider,
+          }}
+        >
+          <SearchBar />
+        </Stack>
+        {<ContactList data={contacts ?? []} {...{ loading: isLoading || isIdle }} />}
+      </div>
+
+      <Button
+        variant="contained"
+        color="secondary"
+  
+        onClick={() => {
+          signOutMutation(null, {
+            onSuccess: () => {
+              navigate("/");
+            },
+          });
         }}
+        sx={{ m: 2,color:theme.palette.getContrastText(theme.palette.secondary.dark) }}
       >
-        <SearchBar />
-      </Stack>
-      { <ContactList data={contacts ?? []} />}
+        Logout
+      </Button>
     </Stack>
   );
 };
