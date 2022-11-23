b920fe18aebef671302dcbb8947f20473b179e14
bertogz <bertogzlx@gmail.com>
2022-11-22 22:53:53 -0800

    continue work on chat route

--- src/Containers/Chat/index.tsx
+++ src/Containers/Chat/index.tsx
@@ -2,6 +2,7 @@
 import { MessageBox, Input } from "react-chat-elements";
 
 import {
+  Avatar,
   Box,
   Button,
   Stack,
@@ -13,9 +14,18 @@ import "react-chat-elements/dist/main.css";
 import { useQueryMessages } from "../../ReactQuery/Queries/useQueryMessages";
 import { useAppSelector } from "../../Redux/useAppSelector";
 import { useQueryContact } from "../../ReactQuery";
-import { useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useMutationSendMessage } from "../../ReactQuery/Mutations/useMutationSendMessage";
+import { PressableText } from "../../Components/ClickableText";
+import {
+  VideoCameraFrontRounded,
+} from "@mui/icons-material";
+import { colorHelper } from "../../Theme";
+const profilepic =
+  "https://www.thesun.co.uk/wp-content/uploads/2022/05/309E522E-D141-11EC-BE62-1280C3EF198F.jpeg";
 
+const imgUrl =
+  "https://img.freepik.com/free-vector/topographic-contour-lines-map-seamless-pattern_1284-52862.jpg?w=2000";
 const TextMessage = ({
   message,
   extraProps,
@@ -69,13 +79,19 @@ const VideoMessage = () => {
     />
   );
 };
+const LoadMoreSection = () => {
+  const [showLoadMore, setShowLoadMore] = useState(true);
+
+  return (
+    <>{showLoadMore && <PressableText>Load Previous Messages</PressableText>}</>
+  );
+};
 const InputSection = ({ dialogId, opponentId }) => {
   const [input, setInput] = useState("");
 
   console.log("input", input);
   const { mutateAsync } = useMutationSendMessage({ dialogId });
   const elRef = useRef();
-  console.log("ref", elRef);
   function onSendMessage() {
     debugger;
     mutateAsync(
@@ -94,6 +110,7 @@ const InputSection = ({ dialogId, opponentId }) => {
         value={input}
         fullWidth
         onChange={(e) => setInput(e.target.value)}
+        InputProps={{ height: "20px" }}
       />
       <Button color="secondary" disabled={!input} onClick={onSendMessage}>
         Send
@@ -111,11 +128,19 @@ const Chat = () => {
   const { _id } = dialog || {};
   const { id: opponent_id } = user || {};
   //const correctDialog = dialogs?.items.find(dialog=>dialog)
-  const { data: messagesResponse } = useQueryMessages({ dialogId: _id });
+  const { data: messagesResponse } = useQueryMessages({
+    dialogId: _id,
+    limit: 50,
+  });
   const { items: messages } = messagesResponse || {};
-  console.log("!!@@messages", messagesResponse);
+  // console.log("!!@@messages", messagesResponse);
+  const messageListRef = useRef();
   const MessageList = useMemo(() => {
-    return messages?.reverse()?.map((message) => {
+    if (!messages?.length) {
+      return undefined;
+    }
+    const messagesCopy = [...messages];
+    return messagesCopy?.reverse()?.map((message) => {
       const { attachments, sender_id } = message || {};
       let messageType: "photo" | "text" = "text";
       if (attachments?.length > 0) {
@@ -131,17 +156,98 @@ const Chat = () => {
       }
     });
   }, [messages]);
+
+  const scrollToBottom = () => {
+    messageListRef?.current?.scrollIntoView({ behavior: "smooth" });
+    const scrollContainer = document.getElementById("scrollContainer");
+    scrollContainer.scrollTo({
+      top: scrollContainer.scrollHeight,
+      left: 0,
+      behavior: "auto",
+    });
+  };
+
+  console.log("messageref", messageListRef);
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages?.length]);
   return (
-    <Box
-      sx={{
-        height: "100%",
-        backgroundColor: theme.palette.secondary.light,
-      }}
-    >
-      <Typography>This is the chat screen</Typography>
-      {MessageList}
-      <InputSection dialogId={_id} opponentId={opponent_id} />
-    </Box>
+    <>
+      <Stack
+        direction="column"
+        sx={{
+          height: "100%",
+          backgroundColor: theme.palette.secondary.light,
+        }}
+      >
+        <Stack
+          direction="row"
+          sx={{
+            backgroundColor: theme.palette.secondary.light,
+            alignItems: "center",
+            borderWidth: 0,
+            borderBottomWidth: "2px",
+            borderStyle: "solid",
+            borderBottomColor: theme.palette.secondary.main,
+            zIndex: 1,
+          }}
+          justifyContent="space-between"
+        >
+          <Stack direction="row" alignItems={"center"} px={2}>
+            <Avatar
+              alt={user?.full_name}
+              src={profilepic}
+              sx={{
+                outlineStyle: "solid",
+                outlineWidth: "2px",
+                outlineColor: theme.palette.secondary.main,
+                margin: 1,
+              }}
+            />
+            <Typography
+              sx={{
+                color: colorHelper.contrastText('secondaryMain'),
+              }}
+            >
+              {user?.full_name}
+            </Typography>
+          </Stack>
+          <Box p={2} >
+            <VideoCameraFrontRounded sx={{color:colorHelper.contrastText('secondaryMain')}} fontSize="large"/>
+          </Box>
+        </Stack>
+        <div
+          id="scrollContainer"
+          key={messages?.length}
+          ref={messageListRef}
+          style={{
+            overflow: "scroll",
+            overscrollBehaviorY: "contain",
+            overflowAnchor: "none",
+            justifyItems: "center",
+            display: "flex",
+            flexDirection: "column",
+            backgroundImage: `url(${imgUrl})`,
+            backgroundSize: "1000px",
+            backgroundPosition: "center center",
+            backgroundRepeat: "repeat",
+            backgroundBlendMode: "soft-light",
+            backgroundColor: theme.palette.secondary.dark,
+          }}
+        >
+          <LoadMoreSection />
+          <Stack
+            direction="column"
+            justifySelf={"flex-end"}
+            maxWidth={"1000px"}
+          >
+            {MessageList}
+          </Stack>
+        </div>
+
+        <InputSection dialogId={_id} opponentId={opponent_id} />
+      </Stack>
+    </>
   );
 };
 export default Chat;
