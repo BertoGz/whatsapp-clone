877382ec636b06e50f0d055718718644ae316a5d
bertogz <bertogzlx@gmail.com>
2022-11-30 10:38:18 -0800

    add staletime default

--- src/ReactQuery/index.ts
+++ src/ReactQuery/index.ts
@@ -4,6 +4,7 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       cacheTime: Infinity,
+      staleTime: Infinity,
       refetchOnWindowFocus: false,
     },
   },
