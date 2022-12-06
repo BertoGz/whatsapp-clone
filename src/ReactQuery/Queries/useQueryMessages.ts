9ff3c0595e6f0b8b4a3705d0376128dcab26d50b
bertogz <bertogzlx@gmail.com>
2022-12-06 12:04:04 -0800

    continue work on useQueryMessages

--- src/ReactQuery/Queries/useQueryMessages.ts
+++ src/ReactQuery/Queries/useQueryMessages.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery, useQuery } from "react-query";
+import { useQuery } from "react-query";
 import { queryClient } from "..";
 import { PromisedQb } from "../../Quickblox";
 import { useAppSelector } from "../../Redux/useAppSelector";
@@ -13,42 +13,76 @@ export const useQueryMessages = (props = { dialogId: "", limit: 100 }) => {
     (state) => state.Quickblox.chatConnected
   );
   const queryKey = ["messages", props.dialogId];
-  const query = useInfiniteQuery<any, any, queryType | undefined>(
-    queryKey,
-    async ({ pageParam: skip = 0 }) => {
-      console.log("skip", skip);
-      const response = await PromisedQb.messagesList({
-        dialogId: props.dialogId,
-        limit: props.limit,
-        skip,
-      });
-      // check if response was ok
-      if (response?.limit) {
-        return response;
+
+  async function getMessagesFn({ pageParam = 0 }) {
+    const queryRef = queryClient.getQueryData(queryKey) as any;
+    const prevMessages = (queryRef?.items || []) as queryType["items"];
+
+    const response = await PromisedQb.messagesList({
+      dialogId: props.dialogId,
+      limit: props.limit,
+      skip: pageParam,
+    });
+
+    // check if response has more
+    const hasMore = response?.items?.length >= props.limit;
+
+    // format the messages by replacing any prev messages with ones caught in the response
+    function formattedMessages() {
+      // update the previous messages with matching new ones
+
+      const newMessages = [] as any;
+      // from new to old
+      let updatedPrevMessages = [...prevMessages];
+
+      // replace any old messages with new payload items
+      // form an array of new messages to append at the start of the stack
+      for (let i = 0; i < response.items?.length; i++) {
+        let messageFound = false;
+        for (let y = 0; y < prevMessages?.length; y++) {
+          if (prevMessages[y]?._id === response?.items[i]?._id) {
+            updatedPrevMessages[y] = response?.items[i];
+            messageFound = true;
+          }
+        }
+        if (!messageFound) {
+          newMessages.push(response?.items[i]);
+        }
       }
-    },
+      debugger;
+      const formatted = [
+        ...newMessages,
+        ...updatedPrevMessages,
+      ] as Array<TypeDataEntityMessage>;
+      return formatted.sort((a, b) => (a?.date_sent < b?.date_sent ? 1 : -1));
+    }
+    if (response?.limit) {
+      debugger;
+      return {
+        ...response,
+        items: formattedMessages(),
+        skip: pageParam,
+        hasMore,
+      };
+    }
+  }
+
+  async function getPrevMessages() {
+    const queryRef = queryClient.getQueryData(queryKey) as any;
+    const skip = queryRef?.items?.length || 0;
+    const res = await getMessagesFn({ pageParam: skip });
+    queryClient.setQueryData(queryKey, res);
+  }
+
+  const query = useQuery<any, any, queryType | undefined>(
+    queryKey,
+    getMessagesFn,
     {
+      staleTime: 0,
       enabled: chatConnected && props.dialogId !== "" && !!props.dialogId,
-      getNextPageParam: (lastResponse) => {
-        // check if more pages exist
-        // returning undefined will set the hasNextPage param to false
-        const hasMore = lastResponse?.items.length >= props.limit;
-        console.log("hasMore", hasMore);
-        if (!hasMore) {
-          return;
-        }
-        // this will return the skip amount to fetch more messages
-        const totalMessageQuery = queryClient.getQueryData(queryKey) as any;
-        const { pages }: { pages: Array<queryType> } = totalMessageQuery || {};
-        if (pages) {
-          const length = pages.reduce(
-            (acc, page) => acc + page.items.length,
-            0
-          );
-          return length;
-        }
-      },
+      getNextPageParam: () => 0,
+      getPreviousPageParam: () => 0,
     }
   );
-  return query;
+  return { ...query, getPrevMessages };
 };
