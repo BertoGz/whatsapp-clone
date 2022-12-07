d2003aad700d6ce4838ea27b8cdf263429e9eb16
bertogz <bertogzlx@gmail.com>
2022-12-07 12:04:07 -0800

    update base_url

--- src/Contants.js
+++ src/Contants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://fast-plains-60517.herokuapp.com/";
 export const GET_MY_RELATIONSHIPS_ENDPOINT = "/get_my_relationships";
 export const SET_RELATIONSHIP_ENDPOINT = "/set_relationship";
