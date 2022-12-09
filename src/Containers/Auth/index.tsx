ffc1e0f26100be2bac3f86c00932cca773ad94df
bertogz <bertogzlx@gmail.com>
2022-12-08 23:58:02 -0800

    continue work on sign up

--- src/Containers/Auth/index.tsx
+++ src/Containers/Auth/index.tsx
@@ -1,9 +1,14 @@
 import {
   Box,
   Button,
+  Card,
   CircularProgress,
+  Divider,
+  FormControl,
+  FormHelperText,
   FormLabel,
   Grid,
+  TextField,
   Typography,
   useTheme,
 } from "@mui/material";
@@ -18,6 +23,9 @@ import { useEffect, useState } from "react";
 import Chat from "../Chat";
 import { useAppSelector } from "../../Redux/useAppSelector";
 import { AppLogo } from "../../Components/AppLogo";
+import { colorHelper } from "../../Theme";
+import { PressableText } from "../../Components/ClickableText";
+import { useMutationUpdateProfile } from "../../ReactQuery/Mutations/useMutationUpdateProfile";
 
 const WelcomeElements = () => {
   return (
@@ -66,54 +74,175 @@ const Auth = () => {
     (state) => state.Quickblox.userSessionValid
   );
   const { mutateAsync: signOutMutation } = useMutationLogout();
-  const { emailVerified, email } = userData || {};
+  const { mutateAsync: updateProfileMutation } = useMutationUpdateProfile();
+  const { emailVerified, email, displayName } = userData || {};
+  const [nameInput, setNameInput] = useState("");
+  const [emailSent, setEmailSent] = useState(false);
   console.log("!!!userData", userData);
   if (!userData) {
     return <></>;
   }
+  if (!displayName && false) {
+    return (
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Card sx={{ maxWidth: "400px", padding: 4 }}>
+          <Typography variant="h6">Finish setting up your profile</Typography>
+          <FormHelperText>Enter your name</FormHelperText>
+          <FormControl error={false}>
+            <TextField
+              fullWidth
+              value={nameInput}
+              onChange={(e) => {
+                setNameInput(e.target.value);
+              }}
+              placeholder="Full name"
+              variant="filled"
+            />
+            {false && <FormHelperText>Enter your name</FormHelperText>}
+          </FormControl>
+          <Typography>Logout</Typography>
+        </Card>
+      </Box>
+    );
+  }
+  function onTrySendEmailVerify() {
+    updateProfileMutation(
+      { displayName: nameInput },
+      {
+        onSuccess: async () => {
+          if (email) {
+            try {
+              await FirebaseActions.sendAuthLinkToEmail(email);
+              setEmailSent(true);
+            } catch (e) {}
+          }
+        },
+      }
+    );
+  }
+  if (!displayName) {
+    return (
+      <Box
+        display="flex"
+        flexDirection={"column"}
+        width={"100%"}
+        alignItems={"center"}
+      >
+        <Card sx={{ maxWidth: "400px", p: 4 }}>
+          <Stack
+            alignItems={"center"}
+            justifyContent="space-evenly"
+            spacing={4}
+          >
+            <div>
+              <AppLogo width="100px" />
+              <Typography>
+                {displayName ? `Hello ${displayName}` : email}
+              </Typography>
+
+              <FormHelperText sx={{ textAlign: "center" }}>
+                Finish setting up your account
+              </FormHelperText>
+            </div>
+
+            {!displayName && (
+              <Box sx={{ width: "100%" }}>
+                <FormHelperText>What do we call you?</FormHelperText>
+
+                <TextField
+                  fullWidth
+                  inputProps={{
+                    style: {
+                      paddingBottom: 0,
+                      paddingTop: 0,
+                      margin: 0,
+                      height: "40px",
+                    },
+                  }}
+                  value={nameInput}
+                  onChange={(e) => {
+                    setNameInput(e.target.value);
+                  }}
+                  placeholder="username"
+                  variant="filled"
+                />
+
+                {false && <FormHelperText>Enter your name</FormHelperText>}
+              </Box>
+            )}
+            <div>
+              <Button
+        
+                disabled={!nameInput && !displayName}
+                variant="contained"
+                onClick={onTrySendEmailVerify}
+                sx={{ textTransform: "none" }}
+              >
+                {emailSent ? `Verification Sent` : `Finish sign-up`}
+              </Button>
+            </div>
+          </Stack>
+        </Card>
+        <Box>
+          <FormLabel>Not you?</FormLabel>
+          <Button
+            onClick={() => {
+              if (email) {
+                signOutMutation(null, {
+                  onSuccess: () => {
+                    navigate("/");
+                  },
+                });
+              }
+            }}
+          >
+            Logout
+          </Button>
+        </Box>
+      </Box>
+    );
+  }
   if (!emailVerified) {
     return (
       <>
-        <Stack alignItems={"center"} justifyContent="space-evenly" spacing={4}>
-          <Box>
-            <Box sx={{ alignItems: "center", justifyItems: "center" }}>
-              <AppLogo />
-            </Box>
-            <Typography variant="h4">Verify your account</Typography>
-          </Box>
-          <Stack direction={"column"}>
-            <FormLabel>{email}</FormLabel>
-            <Button
-              variant="text"
-              onClick={() => {
-                if (email) {
-                  FirebaseActions.sendAuthLinkToEmail(email);
-                }
-              }}
-            >
-              Send Email Verification
-            </Button>
-          </Stack>
-          <Box>
-            <FormLabel>Not you?</FormLabel>
-            <Button
-              onClick={() => {
-                if (email) {
-                  signOutMutation(null, {
-                    onSuccess: () => {
-                      navigate("/");
-                    },
-                  });
-                }
-              }}
-            >
-              Logout
-            </Button>
-          </Box>
-        </Stack>
+        <div>
+          <AppLogo width="100px" />
+
+          <Typography>Verify your email address</Typography>
+          <FormHelperText sx={{ textAlign: "center" }}>
+            We've sent a verification link to {email}
+          </FormHelperText>
+        </div>
+        <Button
+          variant="text"
+          onClick={() => {
+            if (email) {
+              FirebaseActions.sendAuthLinkToEmail(email);
+            }
+          }}
+        >
+          Resend Email Verification
+        </Button>
+        <Box>
+          <FormLabel>Not you?</FormLabel>
+          <Button
+            onClick={() => {
+              if (email) {
+                signOutMutation(null, {
+                  onSuccess: () => {
+                    navigate("/");
+                  },
+                });
+              }
+            }}
+          >
+            Logout
+          </Button>
+        </Box>
       </>
     );
   }
+
   return (
     <>
       {userSessionValid ? (
