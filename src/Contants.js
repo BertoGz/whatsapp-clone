b564ae78adb7f7a46bb17fac50398009770f3ee7
bertogz <bertogzlx@gmail.com>
2022-12-12 13:07:39 -0800

    update consts

--- src/Contants.js
+++ src/Contants.js
@@ -1,5 +1,8 @@
+import { isMobile } from "react-device-detect";
 export const BASE_URL = "https://pure-lake-71547.herokuapp.com"; //"http://localhost:3000"; //"https://pure-lake-71547.herokuapp.com";
 export const GET_MY_RELATIONSHIPS_ENDPOINT = "/get_my_relationships";
 export const SET_RELATIONSHIP_ENDPOINT = "/set_relationship";
 export const SYSTEM_MESSAGE_RELATIONSHIP_ACCEPT =
   "SYSTEM_MESSAGE_RELATIONSHIP_ACCEPT";
+
+export const maxScreenHeight = isMobile ? window.innerHeight : "100vh";
