8d778ac0ede15bbbb9fd99be6cdf71ed1a25a771
bertogz <bertogzlx@gmail.com>
2022-12-13 11:48:23 -0800

    remove logout

--- src/Containers/Contacts/index.tsx
+++ src/Containers/Contacts/index.tsx
@@ -3,16 +3,12 @@ import { Stack } from "@mui/system";
 import ContactList from "../../Components/ContactList";
 import SearchBar from "../../Components/SearchBar";
 import { useQueryContacts } from "../../ReactQuery/Queries/useQueryContacts";
-import { Button, useTheme } from "@mui/material";
-import { useMutationLogout } from "../../ReactQuery/Mutations/useMutationLogout";
-import { useNavigate } from "react-router-dom";
+import { useTheme } from "@mui/material";
 
 const Contacts = () => {
-  const navigate = useNavigate();
   const theme = useTheme();
   const { data: contacts, isLoading } = useQueryContacts();
 
-  const { mutateAsync: signOutMutation } = useMutationLogout();
   return (
     <Stack
       sx={{
@@ -26,26 +22,13 @@ const Contacts = () => {
       <div>
         <SearchBar />
 
-        {<ContactList data={contacts ?? []} {...{ loading: isLoading || contacts === undefined }} />}
+        {
+          <ContactList
+            data={contacts ?? []}
+            {...{ loading: isLoading || contacts === undefined }}
+          />
+        }
       </div>
-
-      <Button
-        variant="contained"
-        color="secondary"
-        onClick={() => {
-          signOutMutation(null, {
-            onSuccess: () => {
-              navigate("/");
-            },
-          });
-        }}
-        sx={{
-          m: 2,
-          color: theme.palette.getContrastText(theme.palette.secondary.dark),
-        }}
-      >
-        Logout
-      </Button>
     </Stack>
   );
 };
